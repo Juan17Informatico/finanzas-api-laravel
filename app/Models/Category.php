2e588e0a1fbd30754dd829d1186a33a0fdbee468
JuanCM <juancampuzano2356@gmail.com>
2025-03-10 17:14:23 -0500

    category(model):
    Se configura el modelo estableciendo relaciones y propiedades fillable

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'type', // 'income' o 'expense'
+    ];
+
+    // Relación: Una categoría puede tener muchas transacciones
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
