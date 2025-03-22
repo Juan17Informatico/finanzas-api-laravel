acc61626331cbbae4f9759469296c8cac6467c1e
JuanCM <juancampuzano2356@gmail.com>
2025-03-22 11:49:32 -0500

    feat(routes api):
    Agrega Endpoints para la autenticación del usuario

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('api')->group(function () {
+    Route::prefix('v1')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/logout', [AuthController::class, 'logout']);
+            Route::get('/me', [AuthController::class, 'me']);
+        });
+    });
 });
