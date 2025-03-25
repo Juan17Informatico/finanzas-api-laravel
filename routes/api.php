a82a921c2165ba8a544af84900c104bd89e6af2c
JuanCM <juancampuzano2356@gmail.com>
2025-03-25 15:49:23 -0500

    feat(routes/api):
    Agrega los endpoint para:
     - 'api/v1/budgets'
     - 'api/v1/categories'
     - 'api/v1/transactions'

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,17 @@
 <?php
 
-use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [\App\Http\Controllers\Api\V1\AuthController::class, 'register']);
+    Route::post('login', [\App\Http\Controllers\Api\V1\AuthController::class, 'login']);
 
     Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::get('/me', [AuthController::class, 'me']);
+        Route::post('/logout', [\App\Http\Controllers\Api\V1\AuthController::class, 'logout']);
+        Route::get('/me', [\App\Http\Controllers\Api\V1\AuthController::class, 'me']);
+
+        Route::apiResource('budgets', \App\Http\Controllers\Api\V1\BudgetController::class);
+        Route::apiResource('categories', \App\Http\Controllers\Api\V1\CategoryController::class);
+        Route::apiResource('transactions', \App\Http\Controllers\Api\V1\TransactionController::class);
     });
 });
