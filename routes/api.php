37c4a8aa14164688765938c7738c046adeb79ab7
JuanCM <juancampuzano2356@gmail.com>
2025-03-27 17:17:48 -0500

    feat(routes/api):
    - Agrega endpoint 'budgets/reports'

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ Route::prefix('v1')->group(function () {
         Route::get('/me', [\App\Http\Controllers\Api\V1\AuthController::class, 'me']);
 
         Route::apiResource('budgets', \App\Http\Controllers\Api\V1\BudgetController::class);
+        Route::get('budgets/reports', [\App\Http\Controllers\Api\V1\BudgetController::class, 'reports']);
         Route::apiResource('categories', \App\Http\Controllers\Api\V1\CategoryController::class);
         Route::apiResource('transactions', \App\Http\Controllers\Api\V1\TransactionController::class);
     });
